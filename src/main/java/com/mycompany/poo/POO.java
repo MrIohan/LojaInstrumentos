1c6440bf68a286f8d141a57e99b69e7b65fec3a8
carpi <carpi@MrCarpivara>
2025-11-17 11:36:10 -0400

    teste2

--- src/main/java/com/mycompany/poo/POO.java
+++ src/main/java/com/mycompany/poo/POO.java
@@ -11,6 +11,6 @@ package com.mycompany.poo;
 public class POO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("teste");
     }
 }
